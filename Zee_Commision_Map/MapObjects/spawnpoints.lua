6e17707f4333b34f527e3b3edb604620b9885eb3
SageAzakaela <sageazakaela@gmail.com>
2023-10-04 15:17:02 -0600

    First Build + Castle

---
+++ Zee_Commision_Map/MapObjects/spawnpoints.lua
@@ -0,0 +1,91 @@
+function SpawnPoints()
+return {
+  accountant = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  bookkeeper = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  burglar = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  cashier = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  chef = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  constructionworker = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  cook = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  customerservice = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  doctor = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  drugdealer = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  farmer = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  fastfoodcook = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  fireofficer = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  itworker = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  janitor = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  militaryofficer = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  militarysoldier = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  nurse = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  officeworker = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  parkranger = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  policeofficer = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  salesperson = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  secretary = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  securityguard = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  shopclerk = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  teacher = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  truckdriver = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  unemployed = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  },
+  waiter = {
+    { worldX = 6, worldY = 17, posX = 147, posY = 27, posZ = 2 }
+  }
+}
+end
